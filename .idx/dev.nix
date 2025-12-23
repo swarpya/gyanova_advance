e0bbd0558b310a31cb165075db93626381eb97c3
swarpyaingavale73 <swarpyaingavale73@gmail.com>
2025-12-23 05:45:41 +0000

    Update dev environment config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,8 +6,15 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
+    pkgs.python311
+    pkgs.python312Packages.conda
+    pkgs.git
+    pkgs.cmake
+    pkgs.clang_18     # BitNet recommends Clang 18+
+    pkgs.python3
+    pkgs.gnumake
+    pkgs.wget
+    pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
